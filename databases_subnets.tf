fe4dbff6629de537c815c18352053f724eb4bd98
Gilmar Alcantara <gilmar.alcantara@hotmart.com>
2024-08-23 01:51:59 -0300

    Fix databases subnet name

--- databases_subnets.tf
+++ databases_subnets.tf
@@ -1,29 +1,29 @@
-resource "aws_subnet" "database_subnet_1a" {
+resource "aws_subnet" "databases_subnet_1a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.51.0/24"
   availability_zone = format("%sa", var.region)
 
   tags = {
-    Name = format("%s-database-subnet-1a", var.project_name)
+    Name = format("%s-databases-subnet-1a", var.project_name)
   }
 }
 
-resource "aws_subnet" "database_subnet_1b" {
+resource "aws_subnet" "databases_subnet_1b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.52.0/24"
   availability_zone = format("%sa", var.region)
 
   tags = {
-    Name = format("%s-database-subnet-1b", var.project_name)
+    Name = format("%s-databases-subnet-1b", var.project_name)
   }
 }
 
-resource "aws_subnet" "database_subnet_1c" {
+resource "aws_subnet" "databases_subnet_1c" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.53.0/24"
   availability_zone = format("%sa", var.region)
 
   tags = {
-    Name = format("%s-database-subnet-1c", var.project_name)
+    Name = format("%s-databases-subnet-1c", var.project_name)
   }
 }
